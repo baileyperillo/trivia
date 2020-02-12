79d31aa7c062d61af9c94c0aa3dc93b651b2fb46
baileyperillo <bperillo2411.student@hsfi.us>
2020-02-12 16:39:02 -0500

    draft 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,20 +4,31 @@ import '../css/App.css';
 import Question from "./Question.jsx"
 import sampleData from "../sample_data.json"
 
+class QuestionBuilder {
+  constructor(question_text, choices, correct_choice_index){
+      this.question_text = question
+      this.choices = choices
+      this.correct_choice_index = correct_choice_index
+  }
+}
 
 class App extends Component {
 
   render() {
-   
+    import {buildFirebase} from '../clients/firebase.js';
+
+    var database = buildFirebase();
+    var databaseRef = database.ref("/questions");
+    databaseRef.once("value").then(function(data) {
+    const questions = data.val();
+
+    console.log(questions);
+
+    const sample = new QuestionBuilder(sampleData)
     return (
       <div className="app">
-        <Question questionData = {sampleData}>
-        
-
-       
-      </Question>
-      
-        
+        <Question questionData = {sample}>       
+        </Question>     
       </div>
     );
   }
